0cfc088ebc7f197ca78e9be4290b249ebbd615d9
austinirwin44 <austin.irwin@student.uva.nl>
2026-02-09 02:58:59 -0500

    chore: initial project setup

---
+++ main.py
@@ -0,0 +1,11 @@
+from dotenv import load_dotenv
+from pydantic import BaseModel
+from langchain_openai import ChatOpenAI
+import os
+
+
+load_dotenv()
+
+model = os.getenv("OPENAI_MODEL", "gpt-5-mini")
+
+
